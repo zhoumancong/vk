4e53132e418a8ce9d1d576a21f7dd85ca8419ef2
zhoumancong <mc_zh@qq.com>
2024-05-28 16:22:25 +0800

    Update vk_adblock.js

--- .github/workflows/vk_adblock.js
+++ .github/workflows/vk_adblock.js
@@ -5,16 +5,15 @@ let obj = JSON.parse(body);
 // 检查和过滤广告对象
 if (obj.response && obj.response.items) {
     obj.response.items = obj.response.items.filter(item => {
-        // 过滤带有 ad 标识的广告内容
-        if (item.ad) {
-            return false;
+        // 过滤带有关于广告商的链接的广告内容
+        if (item.hasOwnProperty('attachments')) {
+            let attachments = item.attachments;
+            for (let attachment of attachments) {
+                if (attachment.link && attachment.link.url && attachment.link.url.startsWith('https://ads.vk.ru')) {
+                    return false;
+                }
+            }
         }
-        
-        // 过滤带有 ERID 的广告内容
-        if (item.ERID) {
-            return false;
-        }
-        
         return true;
     });
 }
