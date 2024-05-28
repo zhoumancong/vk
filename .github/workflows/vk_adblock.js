da3a650afbab3ccd239a4af07380c4baa092c17b
zhoumancong <mc_zh@qq.com>
2024-05-28 15:57:29 +0800

    Update vk_adblock.js

--- .github/workflows/vk_adblock.js
+++ .github/workflows/vk_adblock.js
@@ -2,14 +2,16 @@
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// 移除广告对象
+// 确保正确处理响应结构并移除广告
 if (obj.response && obj.response.items) {
-    obj.response.items = obj.response.items.filter(item => !item.hasOwnProperty('ad_data') && !item.ad);
+    obj.response.items = obj.response.items.filter(item => !item.ad && !item.ad_data);
 }
 
-// 移除广告模块
-if (obj.response && obj.response.ads) {
+// 删除可能的广告字段
+if (obj.response) {
     delete obj.response.ads;
+    delete obj.response.ad;
+    delete obj.response.ad_data;
 }
 
 body = JSON.stringify(obj);
